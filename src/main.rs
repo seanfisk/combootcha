d40ab1e8dac75b7f62303d9007035a8acdb41fc9
Sean Fisk <sean@seanfisk.com>
2020-09-22 21:20:01 -0400

    Basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use anyhow::{Result, anyhow};
+use clap::{crate_authors, crate_description, crate_name};
+
+
 fn is_root() -> bool {
     nix::unistd::Uid::current().is_root()
 }
 
-fn main() {
+fn main() -> Result<()> {
     if !is_root() {
-        eprintln!("This program must be run as root!");
-        std::process::exit(1);
+        return Err(anyhow!("This program must be run as root!"));
     }
+
+    let app = clap::App::new(crate_name!())
+        .about(crate_description!())
+        .author(crate_authors!());
+
+    let _matches = app.get_matches();
+
     println!("Hello, world!");
+
+    Ok(())
 }
