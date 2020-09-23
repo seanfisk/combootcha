6b96047411ac0e3ba62400acded86e3b5c9b84d8
Sean Fisk <sean@seanfisk.com>
2020-09-22 21:13:20 -0400

    Add a root check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn is_root() -> bool {
+    nix::unistd::Uid::current().is_root()
+}
+
 fn main() {
+    if !is_root() {
+        eprintln!("This program must be run as root!");
+        std::process::exit(1);
+    }
     println!("Hello, world!");
 }
