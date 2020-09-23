a71863635e94030d17a41c8219075451959b0115
Sean Fisk <sean@seanfisk.com>
2020-09-23 00:46:37 -0400

    Get Brewfile working!

--- src/homebrew.rs
+++ src/homebrew.rs
@@ -1,19 +1,27 @@
 use crate::verbose_command;
 use anyhow::Result;
 use nix::unistd::{chown, Uid};
-use std::fs::write;
+use std::fs;
+use std::io::Write;
+use std::os::unix::fs::OpenOptionsExt;
 use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::Command;
 use users::User;
 
 pub fn install_formulae(standard_user: User) -> Result<()> {
-    let brewfile_bytes = include_bytes!("Brewfile").to_vec();
+    let brewfile_bytes = include_bytes!("Brewfile");
     // Write the Brewfile to an easy-to-access location so that manual commands can be run against it.
     let brewfile_dest_str = "/usr/local/Brewfile";
     let brewfile_dest = Path::new(brewfile_dest_str);
 
-    write(brewfile_dest, brewfile_bytes)?;
+    let mut brewfile = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .mode(0o400)
+        .open(brewfile_dest)?;
+    brewfile.write_all(brewfile_bytes)?;
+
     chown(
         brewfile_dest,
         Some(Uid::from_raw(standard_user.uid())),
