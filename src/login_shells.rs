9bdf1cea207fe954fe60adc62719f65b9298f10f
Sean Fisk <sean@seanfisk.com>
2020-09-25 15:24:05 -0400

    Grab homebrew bin

--- src/login_shells.rs
+++ src/login_shells.rs
@@ -1,6 +1,26 @@
 use anyhow::Result;
+use log::info;
 use users::User;
 
+// use std::ffi::OsStr;
+// use std::os::unix::ffi::OsStrExt;
+use std::os::unix::process::CommandExt;
+use std::path::Path;
+use std::process::Command;
+
+use crate::verbose_command;
+
 pub(crate) fn set(standard_user: &User) -> Result<()> {
+    info!("Querying Homebrew bin directory");
+    let brew_prefix_output = verbose_command::run_output(
+        Command::new("brew")
+            .arg("--prefix")
+            .uid(standard_user.uid()),
+    )?;
+    let brew_prefix = Path::new(std::str::from_utf8(&brew_prefix_output)?.trim_end_matches('\n'));
+    let brew_bin = brew_prefix.join("bin");
+
+    info!("Homebrew bin directory is {:?}", brew_bin.to_string_lossy());
+
     Ok(())
 }
--- src/verbose_command.rs
+++ src/verbose_command.rs
@@ -1,30 +1,50 @@
 use anyhow::{anyhow, Context, Result};
 use log::info;
 
+use std::io::Write;
 use std::path::Path;
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 
 pub(crate) fn run(command: &mut Command) -> Result<()> {
     run_base(command, None)
 }
 
-// Accept the current_dir so that we can print it. If provided, this will override the Command's current_dir property.
-pub(crate) fn run_in_dir<P: AsRef<Path>>(command: &mut Command, current_dir: P) -> Result<()> {
-    run_base(command, Some(current_dir.as_ref()))
+pub(crate) fn run_output(command: &mut Command) -> Result<Vec<u8>> {
+    // TODO Switch to using subprocess library so that the subprocess can just inherit stderr. We don't want to capture it in 99% of the cases.
+    log_command(&command, None);
+    let output = command
+        .output()
+        .with_context(|| format!("Could not launch process {:?}", command))?;
+    std::io::stderr().write_all(&output.stderr)?;
+    check_status(command, &output.status)?;
+    Ok(output.stdout)
 }
 
+// Accept the current_dir so that we can print it. If provided, this will override the Command's current_dir property.
+// pub(crate) fn run_in_dir<P: AsRef<Path>>(command: &mut Command, current_dir: P) -> Result<()> {
+//     run_base(command, Some(current_dir.as_ref()))
+// }
+
 fn run_base(command: &mut Command, current_dir: Option<&Path>) -> Result<()> {
-    info!(
-        "=> {:?}{}",
-        command,
-        current_dir.map_or("".to_owned(), |d| format!(" (cwd: {:?})", d))
-    );
+    log_command(&command, current_dir);
     if let Some(current_dir) = current_dir {
         command.current_dir(current_dir);
     }
     let status = command
         .status()
         .with_context(|| format!("Could not launch process {:?}", command))?;
+    check_status(command, &status)
+}
+
+fn log_command(command: &Command, current_dir: Option<&Path>) {
+    info!(
+        "=> {:?}{}",
+        command,
+        current_dir.map_or("".to_owned(), |d| format!(" (cwd: {:?})", d))
+    );
+}
+
+fn check_status(command: &Command, status: &ExitStatus) -> Result<()> {
     if status.success() {
         Ok(())
     } else {
