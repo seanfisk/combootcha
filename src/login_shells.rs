8155d338c56d4b158fe93a00c58ef125b3958413
Sean Fisk <sean@seanfisk.com>
2020-09-25 15:37:56 -0400

    reading /etc/shells

--- src/login_shells.rs
+++ src/login_shells.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use log::info;
+use log::{debug, info};
 use users::User;
 
 // use std::ffi::OsStr;
@@ -7,6 +7,7 @@ use users::User;
 use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::Command;
+use std::fs::read_to_string;
 
 use crate::verbose_command;
 
@@ -21,6 +22,16 @@ pub(crate) fn set(standard_user: &User) -> Result<()> {
     let brew_bin = brew_prefix.join("bin");
 
     info!("Homebrew bin directory is {:?}", brew_bin.to_string_lossy());
+
+    let shells_config_path = Path::new("/etc/shells");
+    debug!("Reading {:?}", shells_config_path.to_string_lossy());
+    let shells_config_lines = read_to_string(shells_config_path)?.lines();
+
+    for shell in vec!["bash", "zsh"] {
+        let shell_path = brew_bin.join(shell);
+        info!("Considering adding {:?} to the shells config file", shell_path.to_string_lossy());
+        
+    }
 
     Ok(())
 }
