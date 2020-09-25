753147aa2110c1e186f5748a8202815458b92074
Sean Fisk <sean@seanfisk.com>
2020-09-25 16:01:50 -0400

    Add login shells to /etc/shells

--- src/login_shells.rs
+++ src/login_shells.rs
@@ -1,13 +1,13 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use log::{debug, info};
 use users::User;
 
-// use std::ffi::OsStr;
-// use std::os::unix::ffi::OsStrExt;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::Command;
-use std::fs::read_to_string;
 
 use crate::verbose_command;
 
@@ -25,12 +25,34 @@ pub(crate) fn set(standard_user: &User) -> Result<()> {
 
     let shells_config_path = Path::new("/etc/shells");
     debug!("Reading {:?}", shells_config_path.to_string_lossy());
-    let shells_config_lines = read_to_string(shells_config_path)?.lines();
+    let shells = {
+        let mut set = HashSet::new();
+        for line in BufReader::new(File::open(shells_config_path)?).lines() {
+            let line = line?;
+            if !line.is_empty() && !line.trim_start().starts_with('#') {
+                set.insert(line);
+            }
+        }
+        set
+    };
 
     for shell in vec!["bash", "zsh"] {
         let shell_path = brew_bin.join(shell);
-        info!("Considering adding {:?} to the shells config file", shell_path.to_string_lossy());
-        
+        info!(
+            "Considering adding {:?} to {:?}",
+            shell_path.to_string_lossy(),
+            shells_config_path.to_string_lossy()
+        );
+        let shell_path_str = shell_path
+            .to_str()
+            .ok_or_else(|| anyhow!("Error converting shell path {:?} to a string", shell_path))?;
+        if shells.contains(shell_path_str) {
+            info!(
+                "Shell {:?} is already listed in {:?}",
+                shell_path.to_string_lossy(),
+                shells_config_path.to_string_lossy()
+            );
+        }
     }
 
     Ok(())
