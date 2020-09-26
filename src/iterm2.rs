df42fa7ed98d61c31328469433d31bb98c71495d
Sean Fisk <sean@seanfisk.com>
2020-09-25 20:38:26 -0400

    Create the backgrounds directory using helper

--- src/iterm2.rs
+++ src/iterm2.rs
@@ -1,6 +1,12 @@
 use anyhow::Result;
-use users::User;
+use users::{os::unix::UserExt, User};
+
+use std::path::Path;
 
 pub(crate) fn configure(standard_user: &User) -> Result<()> {
+    let bgs_dir = standard_user
+        .home_dir()
+        .join(Path::new("Library/Application Support/iTerm2/Backgrounds"));
+    crate::fs::ensure_dir_with_owner(bgs_dir, &standard_user)?;
     Ok(())
 }
