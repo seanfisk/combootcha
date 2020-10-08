82d54b6551a1e669cb7e7149e37a01624d013df8
Sean Fisk <sean@seanfisk.com>
2020-10-08 10:19:16 -0400

    Get the conversion started!

--- src/preferences.rs
+++ src/preferences.rs
@@ -6,8 +6,11 @@ use crate::user_defaults::App;
 
 pub(crate) fn set(standard_user: &User) -> Result<()> {
     standard_user.as_user(|| {
-        App::new("com.bluemedora.vROps Deploy")?
-            .bool("TestKey", false)?
+        // Set up clock with day of week, date, and 24-hour clock.
+        App::new("com.apple.menuextra.clock")?
+            .string("DateFormat", "EEE MMM d  H:mm")?
+            .bool("FlashDateSeparators", false)?
+            .bool("IsAnalog", false)?
             .sync()?;
 
         Ok(())
--- src/user_defaults.rs
+++ src/user_defaults.rs
@@ -1,8 +1,8 @@
 use anyhow::{anyhow, Context, Result};
-use log::info;
+use log::{debug, info};
 
 use std::ffi::CString;
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
 // Using core-foundation-rs might also be an option: https://github.com/servo/core-foundation-rs
 //
@@ -11,66 +11,76 @@ use std::fmt::Debug;
 // I'm sure the technique we are using right now isn't as efficient since it goes through C and then Core Foundation, but it has the virtue of being simpler than core-foundation-rs. We're not worried about performance for our purposes.
 
 mod sys {
-    include!(concat!(env!("OUT_DIR"), "/defaults.rs"));
+    include!(concat!(env!("OUT_DIR"), "/user_defaults.rs"));
 }
 
 pub(crate) struct App {
+    id: String,
     c_id: CString,
 }
 
 impl App {
     pub(crate) fn new<S: AsRef<str>>(id: S) -> Result<App> {
-        to_cstring(id.as_ref()).map(|c_id| App { c_id: c_id })
+        to_cstring(id.as_ref()).map(|c_id| {
+            info!("Setting user defaults for application {:?}", id.as_ref());
+            App {
+                id: id.as_ref().to_owned(),
+                c_id: c_id,
+            }
+        })
     }
 
     pub(crate) fn bool(&self, key: &str, value: bool) -> Result<&App> {
-        log_setting("boolean", key, value);
+        self.log_setting("boolean", key, value);
         let c_key = to_cstring(&key)?;
-        unsafe { sys::defaults_set_bool(self.c_id.as_ptr(), c_key.as_ptr(), value) }
+        unsafe { sys::user_defaults_set_bool(self.c_id.as_ptr(), c_key.as_ptr(), value) }
         Ok(self)
     }
 
     pub(crate) fn int(&self, key: &str, value: i64) -> Result<&App> {
-        log_setting("integer", key, value);
+        self.log_setting("integer", key, value);
         let c_key = to_cstring(&key)?;
-        unsafe { sys::defaults_set_i64(self.c_id.as_ptr(), c_key.as_ptr(), value) }
+        unsafe { sys::user_defaults_set_i64(self.c_id.as_ptr(), c_key.as_ptr(), value) }
         Ok(self)
     }
 
     pub(crate) fn float(&self, key: &str, value: f64) -> Result<&App> {
-        log_setting("float", key, value);
+        self.log_setting("float", key, value);
         let c_key = to_cstring(&key)?;
-        unsafe { sys::defaults_set_f64(self.c_id.as_ptr(), c_key.as_ptr(), value) }
+        unsafe { sys::user_defaults_set_f64(self.c_id.as_ptr(), c_key.as_ptr(), value) }
         Ok(self)
     }
 
     pub(crate) fn string(&self, key: &str, value: &str) -> Result<&App> {
-        log_setting("string", key, value);
+        self.log_setting("string", key, value);
         let c_key = to_cstring(&key)?;
-        let c_value = to_cstring(&key)?;
-        unsafe { sys::defaults_set_string(self.c_id.as_ptr(), c_key.as_ptr(), c_value.as_ptr()) }
+        let c_value = to_cstring(&value)?;
+        unsafe {
+            sys::user_defaults_set_string(self.c_id.as_ptr(), c_key.as_ptr(), c_value.as_ptr())
+        }
         Ok(self)
     }
 
     // Would be nice to do this using Drop but it can fail and we want to propagate those failures
     pub(crate) fn sync(&self) -> Result<()> {
         // TODO logging
-        let success = unsafe { sys::defaults_sync(self.c_id.as_ptr()) };
+        let success = unsafe { sys::user_defaults_sync(self.c_id.as_ptr()) };
         if success {
             Ok(())
         } else {
             Err(anyhow!("Defaults synchronization failed"))
         }
     }
+
+    fn log_setting<V: Debug + Display>(&self, type_: &str, key: &str, value: V) {
+        debug!(
+            "Setting application with ID {:?} user defaults key {:?} to {} value {:?}",
+            self.id, key, type_, value
+        );
+        info!("{:?} => {:?}", key, value);
+    }
 }
 
 fn to_cstring(str: &str) -> Result<CString> {
     CString::new(str).with_context(|| format!("Converting string {:?} to CString", str))
-}
-
-fn log_setting<V: Debug>(type_: &str, key: &str, value: V) {
-    info!(
-        "Setting user defaults key {:?} to {} value {:?}",
-        key, type_, value
-    );
 }
