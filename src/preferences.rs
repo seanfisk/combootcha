a2183dc5349b362d946a767996bcd1791f1663e2
Sean Fisk <sean@seanfisk.com>
2020-10-08 10:40:31 -0400

    Add some more

--- src/preferences.rs
+++ src/preferences.rs
@@ -13,6 +13,23 @@ pub(crate) fn set(standard_user: &User) -> Result<()> {
             .bool("IsAnalog", false)?
             .sync()?;
 
+        // Show percentage on battery indicator.
+        //
+        // Note: For some reason, Apple chose the value of ShowPercent to be YES
+        // or NO as a string instead of using a Boolean.
+        App::new("com.apple.menuextra.battery")?
+            .string("ShowPercent", "YES")?
+            .sync()?;
+
+        // Start the character viewer in docked mode. The large window mode doesn't
+        // take focus automatically, and can't AFAIK be focused with any keyboard
+        // shortcut, rendering it less useful for those who like to stay on the
+        // keyboard. The docked mode puts the cursor right in the search field, which
+        // is perfect for keyboard users like myself.
+        App::new("com.apple.CharacterPaletteIM")?
+            .bool("CVStartAsLargeWindow", false)?
+            .sync()?;
+
         Ok(())
     })
 }
