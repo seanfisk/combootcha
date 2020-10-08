4f81e687b44dd88fa0e4b2b3285eeb1b54930b3e
Sean Fisk <sean@seanfisk.com>
2020-10-08 03:42:55 -0400

    Release too

--- src/ffi/defaults.c
+++ src/ffi/defaults.c
@@ -7,15 +7,17 @@ CFStringRef to_cfstring(const char *c_string) {
 }
 
 void defaults_set_bool(const char *app_id, const char *key, bool value) {
-  // TODO Deallocate?
   CFStringRef cf_app_id = to_cfstring(app_id);
   CFStringRef cf_key = to_cfstring(key);
   CFBooleanRef cf_value = value ? kCFBooleanTrue : kCFBooleanFalse;
   CFPreferencesSetAppValue(cf_key, cf_value, cf_app_id);
+  CFRelease(cf_app_id);
+  CFRelease(cf_key);
 }
 
 bool defaults_sync(const char *app_id) {
-  // TODO Deallocate?
   CFStringRef cf_app_id = to_cfstring(app_id);
-  return CFPreferencesAppSynchronize(cf_app_id);
+  bool success = CFPreferencesAppSynchronize(cf_app_id);
+  CFRelease(cf_app_id);
+  return success;
 }
