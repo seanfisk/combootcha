5f1859f4030c5f9f882d6cea74f64397935bf823
Sean Fisk <sean@seanfisk.com>
2020-10-08 20:20:17 -0400

    Right arrow

--- src/user_defaults.rs
+++ src/user_defaults.rs
@@ -78,7 +78,7 @@ impl App {
             "Setting application with ID {:?} user defaults key {:?} to {} value {:?}",
             self.id, key, type_, value
         );
-        info!("{:?} => {:?}", key, value);
+        info!("{:?} → {:?}", key, value);
     }
 }
 
