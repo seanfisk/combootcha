f3c1124b841fea187207360593be56039ecbe2eb
Sean Fisk <sean@seanfisk.com>
2020-10-12 11:57:10 -0400

    Add the rest of Git configurations

--- src/git.rs
+++ src/git.rs
@@ -6,10 +6,34 @@ use crate::verbose_command::Command;
 pub(crate) fn configure(standard_user: &User) -> Result<()> {
     let c = Gitconfig::new(&standard_user);
     c.section(&["user"])
-        .set("name", "Sean Fisk")?
-        .set("email", "sean@seanfisk.com")?;
+        .string("name", "Sean Fisk")?
+        .string("email", "sean@seanfisk.com")?;
     c.section(&["core"])
-        .set("excludesfile", "~/.gitignore-global")?;
+        .string("excludesfile", "~/.gitignore-global")?;
+    c.section(&["alias"])
+        // add all new and changed files in the repo, even if in a subdirectory
+        // according to git-config(1), shell commands are executed from the top of the repository
+        .string("all", "!git add --all")?
+        .string("br", "branch")?
+        .string("ci", "commit")?
+        .string("cia", "commit --all")? // commit all tracked
+        .string("ciam", "commit --all -m")? // commit all tracked with a message
+        .string("cim", "commit -m")? // commit with a message
+        .string("clone", "clone --recursive")?
+        .string("co", "checkout")?
+        .string("cob", "checkout -b")? // create a new branch
+        .string("pr", "remote prune origin")?
+        .string("st", "status")?
+        .string("mod", "ls-files --exclude-standard --modified --others")? // list modified or untracked files
+        .string("sup", "!git submodule init && git submodule update")?
+        .string("tags", "tag -n")? // show tags with their messages
+        .string("nuke", "reset --hard HEAD")?;
+    c.section(&["grep"])
+        .bool("lineNumber", true)?
+        .string("patternType", "perl")?;
+    c.section(&["clean"]).bool("requireForce", false)?;
+    c.section(&["push"]).string("default", "simple")?;
+    c.section(&["submodule"]).bool("recurse", true)?; // Automatically update submodules on 'git checkout'
     Ok(())
 }
 
@@ -40,7 +64,15 @@ impl<'a> Section<'a> {
         }
     }
 
-    fn set(&self, key: &str, value: &str) -> Result<&Section> {
+    fn string(&self, key: &str, value: &str) -> Result<&Section> {
+        self.set(key, None, value)
+    }
+
+    fn bool(&self, key: &str, value: bool) -> Result<&Section> {
+        self.set(key, Some("bool"), &value.to_string())
+    }
+
+    fn set(&self, key: &str, type_: Option<&str>, value: &str) -> Result<&Section> {
         let dotted_path = self
             .path
             .iter()
@@ -48,9 +80,12 @@ impl<'a> Section<'a> {
             .cloned()
             .collect::<Vec<_>>()
             .join(".");
-        Command::new("git")
-            .arg("config")
-            .arg("--global")
+        let mut command = Command::new("git");
+        command.arg("config").arg("--global");
+        if let Some(type_) = type_ {
+            command.arg("--type").arg(type_);
+        }
+        command
             .arg("--")
             .arg(dotted_path)
             .arg(value)
