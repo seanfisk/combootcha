d75627761ec98d3aa4835aa80e5856168ad47ea5
Sean Fisk <sean@seanfisk.com>
2023-01-06 12:52:49 -0500

    Remove some more stuff from Hammerspoon

--- src/hammerspoon-init.lua
+++ src/hammerspoon-init.lua
@@ -28,39 +28,19 @@ for i, key in pairs({[0]="Up", [1]="Down"}) do
   end)
 end
 
-hs.hotkey.bind({"cmd", "alt"}, "w", function()
-    local laptop = "Color LCD"
-    -- These have the same name, so we can't identify them only by name. See here: https://github.com/Hammerspoon/hammerspoon/issues/195
-    local dellVertical = hs.screen.allScreens()[3]
-    local dellHorizontal = hs.screen.allScreens()[1]
-    local top50 = hs.geometry.unitrect(0, 0, 1, 0.5)
-    local bottom50 = hs.geometry.unitrect(0, 0.5, 1, 0.5)
-    hs.layout.apply({
-        {"IntelliJ IDEA", nil, dellHorizontal, hs.layout.maximized, nil, nil},
-        {"Emacs", nil, dellHorizontal, hs.layout.maximized, nil, nil},
-        {"Firefox Developer Editiion", nil, dellVertical, top50, nil, nil},
-        {"iTerm2", nil, dellVertical, bottom50, nil, nil},
-        {"Slack", nil, laptop, hs.layout.maximized, nil, nil},
-        {"Cathode", nil, laptop, hs.layout.maximized, nil, nil},
-    })
-end)
-
-wifiWatcher = nil
-workSsid = "BMPROD"
-lastSsid = hs.wifi.currentNetwork()
-
-function ssidChangedCallback()
-  newSsid = hs.wifi.currentNetwork()
-  if newSsid == workSsid and lastSsid ~= workSsid then
-    -- Arrived at work
-    hs.application.launchOrFocus("Slack")
-    hs.audiodevice.defaultOutputDevice():setMuted(true)
-  elseif newSsid ~= workSsid and lastSsid == workSsid then
-    -- Departed from work
-  end
-
-  lastSsid = newSsid
-end
-
-wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
-wifiWatcher:start()
+-- hs.hotkey.bind({"cmd", "alt"}, "w", function()
+--     local laptop = "Color LCD"
+--     -- These have the same name, so we can't identify them only by name. See here: https://github.com/Hammerspoon/hammerspoon/issues/195
+--     local dellVertical = hs.screen.allScreens()[3]
+--     local dellHorizontal = hs.screen.allScreens()[1]
+--     local top50 = hs.geometry.unitrect(0, 0, 1, 0.5)
+--     local bottom50 = hs.geometry.unitrect(0, 0.5, 1, 0.5)
+--     hs.layout.apply({
+--         {"IntelliJ IDEA", nil, dellHorizontal, hs.layout.maximized, nil, nil},
+--         {"Emacs", nil, dellHorizontal, hs.layout.maximized, nil, nil},
+--         {"Firefox Developer Editiion", nil, dellVertical, top50, nil, nil},
+--         {"iTerm2", nil, dellVertical, bottom50, nil, nil},
+--         {"Slack", nil, laptop, hs.layout.maximized, nil, nil},
+--         {"Cathode", nil, laptop, hs.layout.maximized, nil, nil},
+--     })
+-- end)
