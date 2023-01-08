39b403059440e79949be6e6cdce4063cb48c4dfa
Sean Fisk <sean@seanfisk.com>
2023-01-08 14:34:28 -0500

    Apparently Slack was there all the while

--- src/brewfiles/work.rb
+++ src/brewfiles/work.rb
@@ -4,11 +4,11 @@ brew 'perl' # For installing japi-compliance-checker
 brew 'ruby-build'
 brew 'rbenv'
 brew 'rbenv-default-gems'
-cask 'slack'
 
 # Auto-installed apps:
 # • zoom
 # • firefox
+# • slack
 
 # Java used at VMware
 tap 'homebrew/cask-versions'
