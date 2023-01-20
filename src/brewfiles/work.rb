89b424a6a11b8c20c07a6c52e48bd42eb55c5ae4
Sean Fisk <sean@seanfisk.com>
2023-01-19 19:52:33 -0500

    Add typeit4me for work

--- src/brewfiles/work.rb
+++ src/brewfiles/work.rb
@@ -1,14 +1,15 @@
 # Work only
 
-brew 'perl' # For installing japi-compliance-checker
-brew 'ruby-build'
-brew 'rbenv'
-brew 'rbenv-default-gems'
-
 # Auto-installed apps:
 # • zoom
 # • firefox
 # • slack
+
+brew 'perl' # For installing japi-compliance-checker
+brew 'ruby-build'
+brew 'rbenv'
+brew 'rbenv-default-gems'
+cask 'typeit4me' # Trialing this as a replacement for aText
 
 # Java used at VMware
 tap 'homebrew/cask-versions'
