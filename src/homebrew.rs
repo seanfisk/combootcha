08ede75f4cce344efbc2f01cebb164e2cb97baaf
Sean Fisk <sean@seanfisk.com>
2023-09-30 22:32:54 -0400

    get rid of homebrew install code

--- src/homebrew.rs
+++ src/homebrew.rs
@@ -8,28 +8,6 @@ use crate::user::UserExt as OtherUserExt;
 use crate::verbose_command::Command;
 use crate::Config;
 
-// What I am going to do for now is just instruct the user (me) to install Homebrew manually. It should be a one-and-done thing and then I don't have to worry about use of sudo or non-interactive stuff.
-
-// pub(crate) fn install_system(standard_user: &User) -> Result<()> {
-//     info!("Considering Homebrew installation");
-
-//     if Path::new("/usr/local/bin/brew").exists() {
-//         info!("Hombrew is already installed");
-//         Ok(())
-//     } else {
-//         info!("Installing Homebrew…");
-//         // Yeah, we could pull this down with reqwest, but it's a bit simpler to use the exact command that Hombrew provides
-//         // TODO I think we should consider making this simpler (and possibly using reqwest); the nested shell execution is a bit ugly
-//         Command::new("/bin/bash")
-//             .arg("-c")
-//             .arg("NONINTERACTIVE=1 /bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"")
-//             .user(&standard_user)
-//             .run()?;
-//         info!("Homebrew installed successfully");
-//         Ok(())
-//     }
-// }
-
 pub(crate) fn install_deps(config: Config, standard_user: &User) -> Result<()> {
     info!("Installing Homebrew dependencies via Brewfile");
     let path = standard_user.home_dir().join(".Brewfile");
