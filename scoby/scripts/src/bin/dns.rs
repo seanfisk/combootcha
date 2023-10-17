5363418a179051e4cd4b6b2ca67752f920bb853f
Sean Fisk <sean@seanfisk.com>
2023-10-17 00:34:36 -0400

    implement dns thing

--- scoby/scripts/src/bin/dns.rs
+++ scoby/scripts/src/bin/dns.rs
@@ -1,51 +1,51 @@
-use anyhow::Result;
+// dscacheutil will print no output and exit with a 0 code if it doesn't find
+// the specified name. This is really unclear in the context of using it for DNS
+// lookups, so we are overriding it.
 
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
+use std::io::Write;
+use std::process::{Command, Output};
 
 #[derive(Parser, Debug)]
-#[command(author)]
+#[command(
+    author,
+    about = "Resolve a domain name on macOS using the native DNS facilities."
+)]
 struct Args {
+    #[arg(help = "Domain name to resolve")]
     domain_name: String,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    println!("domain name: {}", args.domain_name);
-    Ok(())
+    let domain_name = args.domain_name;
+
+    let mut command = Command::new("/usr/bin/dscacheutil");
+    command
+        .args(&["-q", "host", "-a", "name"])
+        .arg(&domain_name);
+    let Output {
+        status,
+        stdout,
+        stderr,
+    } = command.output()?;
+
+    if status.success() {
+        if stdout.is_empty() {
+            Err(anyhow!("Could not resolve domain name: {}", domain_name))
+        } else {
+            std::io::stdout()
+                .write_all(&stdout)
+                .context("Writing dscacheutil output to stdout")?;
+            Ok(())
+        }
+    } else {
+        let message = std::str::from_utf8(&stderr)
+            .context("Failure converting dscacheutil stderr to UTF-8")?;
+        Err(anyhow!(
+            "dscacheutil failed with message: {}",
+            message.trim()
+        ))
+    }
 }
-
-/*
-#!/Users/Fisk/.local/bin/python
-# -*- mode: python; -*-
-
-# Python 2/3 compatible
-
-from __future__ import (
-division, absolute_import, print_function, unicode_literals)
-import sys
-import argparse
-import subprocess
-
-arg_parser = argparse.ArgumentParser(description=(
-"Resolve a domain name on macOS using the native DNS facilities."))
-arg_parser.add_argument('domain_name', help='Domain name to resolve')
-args = arg_parser.parse_args()
-
-# dscacheutil will print no output and exit with a 0 code if it doesn't find
-# the specified name. This is really unclear in the context of using it for DNS
-# lookups, so we are overriding it.
-
-output = subprocess.check_output(
-[
-'/usr/bin/dscacheutil',
-'-q', 'host',
-'-a', 'name', args.domain_name,
-    ],
-    universal_newlines=True
-)
-
-if len(output) == 0:
-    sys.exit('Could not resolve domain name: {}'.format(args.domain_name))
-else:
-    print(output, end='')
-*/
