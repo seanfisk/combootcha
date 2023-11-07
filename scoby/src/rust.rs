48dbfef133caeb591382e7d6991d3697948a34c1
Sean Fisk <sean@seanfisk.com>
2023-11-07 14:44:20 -0500

    moar rust stuff

--- scoby/src/rust.rs
+++ scoby/src/rust.rs
@@ -4,19 +4,24 @@ use log::info;
 use users::User;
 
 pub(crate) fn configure(standard_user: User) -> Result<()> {
+    let components: [&str; 2] = [
+        // Linter
+        "clippy",
+        // Language Server Protocol implementation; used for Spacemacs
+        // rls is another component that serves the same purpose but is deprecated in favor of rust-analyzer: https://blog.rust-lang.org/2022/07/01/RLS-deprecation.html
+        "rust-analyzer",
+    ];
+
+    info!("Installing the Rust toolchain using rustup with components {:?}", components);
+
     Command::new("rustup-init")
         .arg("--no-modify-path") // I'll add ~/.cargo/bin to my shell profiles myself
         .arg("-y") // No prompts
-        .args([
-            "--components",
-            "clippy", // Linter
-            // Language Server Protocol implementation; used for Spacemacs
-            // rls is another component that serves the same purpose but is deprecated in favor of rust-analyzer: https://blog.rust-lang.org/2022/07/01/RLS-deprecation.html
-            "rust-analyzer",
-        ])
+        .arg("--component")
+        .args(components)
         .user(standard_user)
         .run()?;
 
-    info!("Rust installed with components successfully!");
+    info!("Rust installed or updated successfully!");
     Ok(())
 }
