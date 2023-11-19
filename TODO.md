5c3ac04368f067c0bacffc74bb1416ab78db6fd1
Sean Fisk <sean@seanfisk.com>
2023-11-19 11:14:53 -0500

    fix ssh host * configurations

--- TODO.md
+++ TODO.md
@@ -10,7 +10,6 @@
 - When transferring to a new computer, note that `combootcha` needs to be right-click Opened before it can be run through terminal
 - Move mas stuff to the end of `Brewfile` as it is best to have LastPass or Bitwarden before App Store installs
 - Add aText or remove from setup (for now)
-- SSH: `Host *` should be at the end of the file
 - Sign in to App Store (needed for Microsoft To Do, but also a thing we should just do)
 - Set up iCloud
 - Sign into Firefox sync
--- scoby/src/ssh/mod.rs
+++ scoby/src/ssh/mod.rs
@@ -12,11 +12,13 @@ pub(crate) fn configure(standard_user: &User, config_extra_bytes: Option<&[u8]>)
     standard_user.as_effective_user(|| {
         crate::fs::ensure_dir(&ssh_dir)?;
         let mut file = crate::fs::create_file(&path)?;
-        file.write_all(include_bytes!("config"))?;
+        file.write_all(include_bytes!("config/pre"))?;
         if let Some(bytes) = config_extra_bytes {
             file.write_all(b"\n")?;
             file.write_all(bytes)?;
         }
+        file.write_all(b"\n")?;
+        file.write_all(include_bytes!("config/post"))?;
         file.sync_all()?;
         Ok(())
     })
