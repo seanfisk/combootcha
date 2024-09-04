9b45105461f5c12b6f105216781cda74778ef262
Sean Fisk <sean@seanfisk.com>
2024-09-03 23:00:06 -0400

    Completely remove LastPass
    Obviously, removing LastPass doesn't delete my LastPass account, which is still
    accessible on the web.
    
    Closes #40.

--- combootcha/src/main.rs
+++ combootcha/src/main.rs
@@ -25,24 +25,6 @@ fn main() -> Result<()> {
         /*hammerspoon_init_lua_extra_bytes=*/ None,
     )?;
 
-    standard_user.as_effective_user(|| {
-        let lastpass_cmd_shift_key = "1179914";
-        scoby::user_defaults::App::new("com.lastpass.LastPass")?
-            // Some preferences are prefixed by a hash, which seems to be stored in
-            // 'lp_local_pwhash'. We don't know what that hash means, or whether it's
-            // consistent, so just leave those alone.
-            .string("global_StartOnLogin", "1")?
-            // ⌘⇧L
-            .string("global_SearchHotKeyMod", lastpass_cmd_shift_key)?
-            .string("global_SearchHotKeyVK", "37")?
-            // ⌘⇧V
-            .string("global_VaultHotKeyMod", lastpass_cmd_shift_key)?
-            .string("global_VaultHotKeyVK", "9")?
-            .sync()?;
-
-        Ok(())
-    })?;
-
     info!("Setup complete!");
 
     Ok(())
