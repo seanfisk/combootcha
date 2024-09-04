7cc300a504f06a44eb21af78f43b72ef9d2a9df3
Sean Fisk <sean@seanfisk.com>
2024-09-03 23:02:12 -0400

    cleaner

--- combootcha/src/main.rs
+++ combootcha/src/main.rs
@@ -1,13 +1,10 @@
 use anyhow::Result;
 use log::info;
-use scoby::UserExt;
 
 fn main() -> Result<()> {
-
     let (scoby_cli, app) = scoby::Cli::init()?;
     let matches = app.get_matches();
     let mut global_config = scoby_cli.parse_config(&matches)?;
-    let standard_user = global_config.standard_user.clone();
 
     global_config.zsh.add_profile_content("# extra stuff\n");
     global_config
