05d3e0ddc53a02f4dd9ffc33b09904c80bf8e671
Sean Fisk <sean@seanfisk.com>
2026-04-16 17:55:53 -0400

    Improve formatting of pmset command

--- scoby/src/power_management.rs
+++ scoby/src/power_management.rs
@@ -4,17 +4,11 @@ use log::info;
 
 pub(crate) fn configure() -> Result<()> {
     info!("Configuring power management preferences");
-    let mut command = Command::new("pmset");
-    command.arg("-a"); // Apply to all states (battery, charger, & UPS)
-    for (name, value) in [
-        ("womp", "0"),     // Disable 'Wake for network access'
-        ("powernap", "0"), // Disable Power Nap, which awakens the computer to check email, etc. Annoying.
-        ("acwake", "0"),   // Don't wake up when charger is plugged in
-        ("lidwake", "1"),  // Wake up when lid is opened
-    ] {
-        command.arg(name);
-        command.arg(value);
-    }
-    command.run()?;
-    Ok(())
+    Command::new("pmset")
+        .arg("-a") // Apply to all states (battery, charger, & UPS)
+        .args(["womp", "0"]) // Disable 'Wake for network access'
+        .args(["powernap", "0"]) // Disable Power Nap, which awakens the computer to check email, etc. Annoying.
+        .args(["acwake", "0"]) // Don't wake up when charger is plugged in
+        .args(["lidwake", "1"]) // Wake up when lid is opened
+        .run()
 }
